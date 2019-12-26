db3d5c161dec7deed4af55f1608a7f714b3d847d
Yasuo Nakamura <levtrian@gmail.com>
2019-12-26 15:38:05 +0900

    Add RestaurantViewController and AFNetworking

---
+++ FindRestaurantsNearMe/FindRestaurantsNearMe/RestaurantListViewControllerTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  RestaurantListViewControllerTableViewController.h
+//  FindRestaurantsNearMe
+//
+//  Created by Yasuo Nakamura on 2019/12/26.
+//  Copyright © 2019 Yasuo Nakamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RestaurantListViewControllerTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
